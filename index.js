41b828a79c5491adfaab5298fc0f1b5bf5c45d40
Walter Radduso <w.radduso@gmail.com>
2023-04-28 21:10:36 -0300

    update lib, rules and readme

--- index.js
+++ index.js
@@ -1,116 +1,105 @@
 module.exports = {
   root: true,
-  extends: [
-    "eslint:recommended",
-    "next/core-web-vitals",
-    "airbnb",
-    "plugin:prettier/recommended"
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: ['airbnb-typescript/base', 'eslint:recommended', 'next/core-web-vitals', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module"
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: [
-    "@typescript-eslint",
-    "import",
-    "prettier",
-    "react",
-    "react-hooks"
-  ],
+  plugins: ['@typescript-eslint', 'import', 'prettier', 'react', 'react-hooks'],
   rules: {
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never"
-      }
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
     ],
-    "import/no-extraneous-dependencies": "off",
-    "import/prefer-default-export": "off",
-    "no-console": "warn",
-    "react/button-has-type": "off",
-    "react-hooks/rules-of-hooks": "off",
-    "react/jsx-filename-extension": [
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'no-console': 'warn',
+    'react/button-has-type': 'off',
+    'react-hooks/rules-of-hooks': 'off',
+    'react/jsx-filename-extension': [
       1,
       {
-        extensions: [".jsx", ".tsx"]
-      }
+        extensions: ['.jsx', '.tsx'],
+      },
     ],
-    "react/jsx-uses-react": "off",
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/self-closing-comp": "warn",
-    "prettier/prettier": [
-      "error",
+    'react/jsx-uses-react': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': 'warn',
+    'prettier/prettier': [
+      'error',
       {
-        arrowParens: "always",
+        arrowParens: 'always',
         bracketSpacing: true,
-        endOfLine: "auto",
+        endOfLine: 'auto',
         jsxBracketSameLine: false,
         printWidth: 140,
         semi: true,
         singleQuote: true,
         tabWidth: 2,
-        trailingComma: "all"
-      }
+        trailingComma: 'all',
+      },
     ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
       {
-        args: "after-used",
-        argsIgnorePattern: "^_.*?$",
-        ignoreRestSiblings: false
-      }
+        args: 'after-used',
+        argsIgnorePattern: '^_.*?$',
+        ignoreRestSiblings: false,
+      },
     ],
-    "import/order": [
-      "warn",
+    'import/order': [
+      'warn',
       {
         allowSeparatedGroups: true,
-        groups: ["builtin", "external", "index", "internal", "object", "parent", "sibling", "type"],
+        groups: ['builtin', 'external', 'index', 'internal', 'object', 'parent', 'sibling', 'type'],
         ignoreCase: true,
         ignoreDeclarationSort: true,
         ignoreMemberSort: false,
-        memberSyntaxSortOrder: ["all", "single", "multiple", "none"],
-        "newlines-between": "always",
+        memberSyntaxSortOrder: ['all', 'single', 'multiple', 'none'],
+        'newlines-between': 'always',
         pathGroups: [
           {
-            group: "external",
-            pattern: "~/**",
-            position: "after"
-          }
-        ]
-      }
+            group: 'external',
+            pattern: '~/**',
+            position: 'after',
+          },
+        ],
+      },
     ],
-    "react/jsx-sort-props": [
-      "warn",
+    'react/jsx-sort-props': [
+      'warn',
       {
         callbacksLast: true,
         noSortAlphabetically: false,
         reservedFirst: true,
-        shorthandFirst: true
-      }
+        shorthandFirst: true,
+      },
     ],
-    "padding-line-between-statements": [
-      "warn",
+    'padding-line-between-statements': [
+      'warn',
       {
-        blankLine: "always",
-        prev: "*",
-        next: "return"
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
       },
       {
-        blankLine: "always",
-        prev: ["const", "let", "var"],
-        next: "*"
+        blankLine: 'always',
+        prev: ['const', 'let', 'var'],
+        next: '*',
       },
       {
-        blankLine: "any",
-        prev: ["const", "let", "var"],
-        next: ["const", "let", "var"]
-      }
-    ]
-  }
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+    ],
+  },
 };
